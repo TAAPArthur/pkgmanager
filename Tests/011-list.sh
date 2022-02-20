776ec3d6b8c26bb4c0bb612ef137ae8c34090e76
Arthur Williams <taaparthur@disroot.org>
2022-02-20 15:23:37 -0800

    Add proper tests for list

---
+++ Tests/011-list.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+$PKG_CMD list
+$PKG_CMD new A
+$PKG_CMD new B
+$PKG_CMD new C
+$PKG_CMD b A B C
+$PKG_CMD i A B C
+
+$PKG_CMD list A
+$PKG_CMD list A | grep A
+$PKG_CMD list A B | grep B
+$PKG_CMD list A B | grep -v C
+$PKG_CMD list A B C | grep A
+
+$PKG_CMD list | grep A
+$PKG_CMD list | grep B
+$PKG_CMD list | grep C
+
