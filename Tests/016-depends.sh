d45c6e2fc1232413a5f61317eb121e896afb9eef
Arthur Williams <taaparthur@disroot.org>
2022-02-27 03:25:53 -0800

    Allow looking up depedencies for uninstalled packages

--- Tests/016-depends.sh
+++ Tests/016-depends.sh
@@ -4,12 +4,21 @@ create_factor_packages
 
 $PKG_CMD b 4
 $PKG_CMD i 4
-$PKG_CMD depends 4
-for i in $(seq 1 12); do
-    echo "Checking $i"
-    if grep -q "$i" 4/depends; then
-        $PKG_CMD depends 4 | grep "$i"
-    else
-        $PKG_CMD depends 4 | grep -v "$i"
-    fi
-done
+mkdir dummy
+cd dummy
+check() {
+    pkg=$1
+    for i in $(seq 1 12); do
+        echo "Checking $i"
+        if grep -q "$i" "../$pkg/depends"; then
+            $PKG_CMD depends "$pkg" | grep "$i"
+        else
+            $PKG_CMD depends "$pkg" | grep -v "$i"
+        fi
+    done
+}
+
+check 2
+check 4
+check 8
+check 12
