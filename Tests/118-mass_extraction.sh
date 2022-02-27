e2c04f8a4087a4c12f66bc6275b833d67170d8fc
Arthur Williams <taaparthur@disroot.org>
2022-02-26 19:23:13 -0800

    Fully test source extraction

---
+++ Tests/118-mass_extraction.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -e
+
+
+mkdir git_repo
+( cd git_repo; git init; touch gitFile; git add .; git  -c user.name='Paul Draper' -c user.email='my@email.org' commit -m"Init")
+
+mkdir tarDir
+touch tarDir/tarFile
+tar -c tarDir > tarFile.tar
+
+
+mkdir tarDirCompressed
+touch tarDirCompressed/tarFileCompressed
+tar -c tarDirCompressed | gzip > tarFile.tar.gz
+tar -tf  tarFile.tar.gz
+
+
+mkdir zipDir
+touch zipDir/zipFile
+
+zip -r zipFile zipDir
+
+
+$PKG_CMD new A 1 "git+file://$PWD/git_repo" "file://$PWD/tarFile.tar" "file://$PWD/tarFile.tar.gz" "file://$PWD/zipFile.zip" "files/file"
+set -x
+mkdir A/files
+touch A/files/file
+
+cat - <<EOF >> A/build
+set -ex
+[ -r gitFile ]
+[ -r tarFile ]
+[ -r tarFileCompressed ]
+[ -r zipFile ]
+EOF
+
+$PKG_CMD b A
+$PKG_CMD i A
+
+
