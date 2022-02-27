48a037bc6431310d6de256f165b3aef2734d5323
Arthur Williams <taaparthur@disroot.org>
2022-02-26 17:53:10 -0800

    Convert owns into an extension

---
+++ Tests/052-owns.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+$PKG_CMD list
+$PKG_CMD new A
+$PKG_CMD new B
+echo 'touch $1/A_file' >> A/build
+echo 'touch $1/B_file' >> B/build
+touch "$PKGMAN_ROOT/not_A_file"
+touch "$PKGMAN_ROOT/not_B_file"
+
+$PKG_CMD b A B
+$PKG_CMD i A B
+
+[ "$($PKG_CMD owns "A_file")" = A ]
+[ "$($PKG_CMD owns "B_file")" = B ]
+[ -z "$($PKG_CMD owns "not_a_file")" ]
+$PKG_CMD owns "not_A_file" && exit 1
+$PKG_CMD owns "not_B_file" && exit 1
+$PKG_CMD owns "not_a_file" && exit 1
+exit 0
