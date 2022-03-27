a15ebb9b1846b972068e03670130f8415adf05be
Arthur Williams <taaparthur@disroot.org>
2022-03-26 18:53:41 -0700

    search: Use basename of given pattern
    When searching, one may give a path instead of just a file name. We'll
    now implicitly take the basename of any path provided.

--- Tests/001-search.sh
+++ Tests/001-search.sh
@@ -3,4 +3,6 @@
 $PKG_CMD new AAAB
 [ "$(basename "$($PKG_CMD s "A.*")")" = AAAB ]
 [ "$(basename "$($PKG_CMD s ".*B")")" = AAAB ]
+[ "$(basename "$($PKG_CMD s "AAAB/")")" = AAAB ]
+[ "$(basename "$($PKG_CMD s "$PWD/AAAB/")")" = AAAB ]
 ! $PKG_CMD s "badname" || exit 1
