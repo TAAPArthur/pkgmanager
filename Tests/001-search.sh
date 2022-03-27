6686cc555070bade3cecee0539789aefd349ba3d
Arthur Williams <taaparthur@disroot.org>
2022-03-27 01:34:20 -0700

    search: only return packages that match the pattern exactly

--- Tests/001-search.sh
+++ Tests/001-search.sh
@@ -5,4 +5,6 @@ $PKG_CMD new AAAB
 [ "$(basename "$($PKG_CMD s ".*B")")" = AAAB ]
 [ "$(basename "$($PKG_CMD s "AAAB/")")" = AAAB ]
 [ "$(basename "$($PKG_CMD s "$PWD/AAAB/")")" = AAAB ]
+! $PKG_CMD s "A" || exit 1
+! $PKG_CMD s "B" || exit 1
 ! $PKG_CMD s "badname" || exit 1
