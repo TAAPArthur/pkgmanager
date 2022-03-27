b8fbe848bca64e5a28bf45fa2cc2fd9f6496f813
Arthur Williams <taaparthur@disroot.org>
2022-03-26 22:00:59 -0700

    outdated: don't report every package as outdated

--- Tests/014-outdated.sh
+++ Tests/014-outdated.sh
@@ -1,8 +1,14 @@
 #!/bin/sh -e
 
 $PKG_CMD new A
-$PKG_CMD b A
-$PKG_CMD i A
+$PKG_CMD new B
+$PKG_CMD b A B
+$PKG_CMD i A B
+mkdir temp
+cd temp
 set -x
-echo "1 2" > A/version
+$PKG_CMD outdated
+[ -z "$($PKG_CMD outdated)" ]
+echo "1 2" > ../A/version
 $PKG_CMD outdated A | grep "A"
+$PKG_CMD outdated A | grep -v "B"
