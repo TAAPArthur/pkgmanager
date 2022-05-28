bec9d992d5db2dfac437729c4df44f9ae45fddb1
Arthur Williams <taaparthur@disroot.org>
2022-05-28 00:31:49 -0700

    Drastically speed up depend detection

--- hooks/50-depend_detection.sh
+++ hooks/50-depend_detection.sh
@@ -4,19 +4,15 @@ case "$TYPE" in
     post-build)
         METADATA=$(PKGMAN_ROOT="$PWD" pkgmanager get-metadata-install-dir "$PKG")
         TEMP_DEPENDS="$WORKING_DIR/.temp"
-        for dir in */sbin/* */bin/* */lib*/*; do
-            if [ -e "$dir" ]; then
-                find "$dir" -type f -exec readelf -d {} \; 2>/dev/null | sed -n "s/.*Shared library: \[\([^\]*\)]/\1/p" | while read -r lib; do
-                    for path in $PKGMAN_ROOT/usr/lib/"$lib" $PKGMAN_ROOT/usr/lib/"$lib"; do
-                        [ -f "$path" ] || continue
-                        owner="$(pkgmanager owns "${path#"$PKGMAN_ROOT"}" || echo "$PKG")"
-                        if [ "$owner" != "$PKG" ]; then
-                            echo "$owner" >> "$TEMP_DEPENDS"
-                        fi
-                    done
+        find usr/ -type f -perm 755 -exec readelf -d {} \+ 2>/dev/null | sed -n "s/.*Shared library: \[\([^\]*\)]/\1/p" | sort -u |
+            while read -r lib; do
+                find "$PKGMAN_ROOT/usr/lib" -type f -name "$lib" | while read -r path; do
+                    owner="$(pkgmanager owns "${path#"$PKGMAN_ROOT"}" || echo "$PKG")"
+                    if [ "$owner" != "$PKG" ]; then
+                        echo "$owner" >> "$TEMP_DEPENDS"
+                    fi
                 done
-            fi
-        done
+            done
         if [ -e "$TEMP_DEPENDS" ]; then
             touch "$METADATA/depends"
             sort -u "$METADATA/depends" "$TEMP_DEPENDS" > "${TEMP_DEPENDS}.temp"
