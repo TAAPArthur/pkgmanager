e92f0fea591e8371d77a5524f68137fa5fd023ef
Arthur Williams <taaparthur@disroot.org>
2022-05-28 09:23:29 -0700

    conflicts: speed up conflicts detection

--- hooks/50-conflicts.sh
+++ hooks/50-conflicts.sh
@@ -4,7 +4,11 @@ case "$TYPE" in
     pre-install)
         pkg_gen_manifest | { if [ -r "$3" ]; then comm -23 - "$3"; else cat -; fi; } | while read -r path; do
             # If package is owned by another package
-            if [ -e "$PKGMAN_ROOT/$path" ] && [ ! -d "$PKGMAN_ROOT/$path" ] && [ "$(pkgmanager owns "$path" || echo "$PKG")" != "$PKG" ]; then
+            if [ -e "$PKGMAN_ROOT/$path" ] && [ ! -d "$PKGMAN_ROOT/$path" ] ; then
+               echo "$path"
+            fi
+        done | pkgmanager owns - | while read -r owner path; do
+            if [ "$PKG" != "$owner" ]; then
                 # if you don't want conflict management either
                 # (1) exit with non-zero status to abort the install
                 # (2) rm "$path"
