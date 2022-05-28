dc1d16e71aeb7de205cc856c690b8873f44d8626
Arthur Williams <taaparthur@disroot.org>
2022-05-27 23:28:39 -0700

    remove: sort arguments to remove pkgs in a sane order
    If there is a dependency chain like A -> B -> C, then
    `pkgmanager remove C A B` now works. We'll remove the packages with the
    most dependencies first which is the exact opposite way in which we
    build packages.

--- Tests/017-remove_deps.sh
+++ Tests/017-remove_deps.sh
@@ -8,3 +8,10 @@ $PKG_CMD i 12
 $PKG_CMD revdepends 4 | grep 12
 $PKG_CMD r 4 && exit 1
 $PKG_CMD list 4
+
+$PKG_CMD r 4 2 12 6
+! $PKG_CMD list 4 || exit 1
+! $PKG_CMD list 2 || exit 1
+! $PKG_CMD list 12 || exit 1
+$PKG_CMD list 1
+$PKG_CMD list 3
