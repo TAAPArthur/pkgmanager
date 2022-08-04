6b8c0af11bbb5d9c493909f153044d8a79eeb3d9
Arthur Williams <taaparthur@disroot.org>
2022-08-03 18:58:48 -0700

    Support checking out a specific git branch
    Now git sources can be appended with "@BRANCH_NAME" and then
    BRANCH_NAME will be downloaded its hash will be used for versioning

--- Tests/102-update_git.sh
+++ Tests/102-update_git.sh
@@ -11,16 +11,30 @@ hash="$(cd git_repo; git rev-parse HEAD)"
 $PKG_CMD list A
 $PKG_CMD list A | grep "$hash"
 
-
 ( cd git_repo; echo "A" > file; git add .; git -c user.name='Test' -c user.email='user@test.com' commit -m"2")
 
-
-hash="$(cd git_repo; git rev-parse HEAD)"
+hashA="$(cd git_repo; git rev-parse HEAD)"
 
 [ -z "$($PKG_CMD outdated)" ]
 echo y | $PKG_CMD update git
-$PKG_CMD list A
-$PKG_CMD list A | grep "$hash"
+$PKG_CMD list A | grep "$hashA"
 
 [ -z "$($PKG_CMD outdated)" ]
+[ -z "$($PKG_CMD outdated git)" ]
+
+( cd git_repo; git checkout -B "new_branch"; echo "B" > file; git add .; git -c user.name='Test' -c user.email='user@test.com' commit -m"2"; )
+
+hashB="$(cd git_repo; git rev-parse HEAD)"
+( cd git_repo; git checkout master)
+
+[ -z "$($PKG_CMD outdated)" ]
+[ -z "$($PKG_CMD outdated git)" ]
+
+$PKG_CMD new B git "git+file://$PWD/git_repo@new_branch"
+$PKG_CMD b B
+$PKG_CMD i B
+
+$PKG_CMD list A | grep "$hashA"
+$PKG_CMD list B | grep "$hashB"
+
 [ -z "$($PKG_CMD outdated git)" ]
